5a0296c33e151573ac0ad606439f1381f9492837
Vojta Vorel <vorelvo2@uhk.cz>
2025-10-02 16:32:13 +0200

    Po cviku 2

--- src/main/java/uprom/Cviko02_Uloha01.java
+++ src/main/java/uprom/Cviko02_Uloha01.java
@@ -7,7 +7,27 @@ import java.util.Scanner;
 // Scanner input = new Scanner(System.in);
 public class Cviko02_Uloha01
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double a;
+        double b;
+        double c;
+        a = input.nextDouble();
+        b = input.nextDouble();
+        c = input.nextDouble();
+        if (a == 0) {
+            System.out.println("Není kvadratická rovnice");
+        } else {
+            double d;
+            d = b * b - 4 * a * c;
+            if (d < 0) {
+                System.out.println("Nemá reálné řešení");
+            } else {
+                double x1; double x2;
+                x1 = (-b - Math.sqrt(d)) / (2 * a);
+                x2 = (-b + Math.sqrt(d)) / (2 * a);
+                System.out.println(x1 + ", " + x2);
+            }
+        }
     }
 }
--- src/main/java/uprom/Cviko02_Uloha03.java
+++ src/main/java/uprom/Cviko02_Uloha03.java
@@ -5,5 +5,10 @@ public class Cviko02_Uloha03
 {
     public static void main(String[] args)
     {
+        float f1 = 44684666664f;
+        double d = 3464464d;
+        long l1 = 46846648466L;
+        int x = 56;
+        int y = x + -2;
     }
 }
