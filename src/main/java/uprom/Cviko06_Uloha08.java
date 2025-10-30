dd1df2a6536e08afed20668df1c614a30a771625
Vojta Vorel <vorel.vojtech@gmail.com>
2025-10-30 10:04:31 +0100

    Příprava na cviko 6

---
+++ src/main/java/uprom/Cviko06_Uloha08.java
@@ -0,0 +1,23 @@
+package uprom;
+
+import java.util.Scanner;
+
+public class Cviko06_Uloha08
+{
+    public static void main(String[] args)
+    {
+        Scanner s = new Scanner(System.in);
+        int yearA =  s.nextInt();// např. 2018;
+        int monthA = s.nextInt(); // např. 12;
+        int yearB = s.nextInt();// např. 2017;
+        int monthB = s.nextInt();// např. 11;
+        if(true)   // Doplň
+        {
+            System.out.println("A je starší než B");
+        }
+        else
+        {
+            System.out.println("A není starší než B");
+        }
+    }
+}
